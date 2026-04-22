eb89a7a953db0ddafab188c1e95b7fd812f76777
Ryoukaii <budiimamprsty@gmail.com>
2026-04-22 18:45:08 +0700

    refactor: rename linked list files and enable age classification logic

--- LEC/latihan/usia.c
+++ LEC/latihan/usia.c
@@ -9,28 +9,28 @@ int main()
     hitungUsia = 2024 - tahunLahir;
 
     // cara saya
-    // if (tahunLahir > 2024 || tahunLahir < 1900)
-    // {
-    //     printf("Tahun lahir tidak valid\n");
-    //     return 0;
-    // }else{
-    //     if (hitungUsia >= 0 && hitungUsia <= 5){
-    //         printf("Balita berumur %d tahun\n", hitungUsia);
-    //     }
-    //     else if (hitungUsia > 5 && hitungUsia <= 12){
-    //         printf("Anak-anak berumur %d tahun\n", hitungUsia);
-    //     }
-    //     else if (hitungUsia > 12 && hitungUsia <= 18){
-    //         printf("Remaja berumur %d tahun\n", hitungUsia);
-    //     }
-    //     else if (hitungUsia > 18 && hitungUsia < 60){
-    //         printf("Dewasa berumur %d tahun\n", hitungUsia);
-    //     }
-    //     else if (hitungUsia >= 60){
-    //         printf("Lansia berumur %d tahun\n", hitungUsia);
-    //     }
-    //     return 0;
-    // }
+    if (tahunLahir > 2024 || tahunLahir < 1900)
+    {
+        printf("Tahun lahir tidak valid\n");
+        return 0;
+    }else{
+        if (hitungUsia >= 0 && hitungUsia <= 5){
+            printf("Balita berumur %d tahun\n", hitungUsia);
+        }
+        else if (hitungUsia > 5 && hitungUsia <= 12){
+            printf("Anak-anak berumur %d tahun\n", hitungUsia);
+        }
+        else if (hitungUsia > 12 && hitungUsia <= 18){
+            printf("Remaja berumur %d tahun\n", hitungUsia);
+        }
+        else if (hitungUsia > 18 && hitungUsia < 60){
+            printf("Dewasa berumur %d tahun\n", hitungUsia);
+        }
+        else if (hitungUsia >= 60){
+            printf("Lansia berumur %d tahun\n", hitungUsia);
+        }
+        return 0;
+    }
 
     // // cara ihsan
     // if (tahunLahir > 2024 || tahunLahir < 1900)
